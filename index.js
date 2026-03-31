2e256af03f420b6929a028312313e91d965f8a49
E.Altantulga <dug.ndaatgal1@gmail.com>
2026-03-31 12:37:11 +0800

    add health endpoint

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+app.get("/health", (req, res) => res.status(200).send("OK"));
+
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const GROQ_API_KEY = process.env.GROQ_API_KEY;
